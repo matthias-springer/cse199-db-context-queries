436a94418434268b7694e0575db555773e31fa2d
Matthias Springer <me@matthiasspringer.de>
2014-05-10 17:45:18 -0700

    Initial Commit

---
+++ context/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  context
+//
+//  Created by Matthias on 5/10/14.
+//  Copyright (c) 2014 ucsd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
