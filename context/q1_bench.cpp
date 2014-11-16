e1f02bc300f3fd039dcb1d7fbf0ebc24eb9a82f1
Matthias Springer <me@matthiasspringer.de>
2014-11-16 13:27:11 +0100

    Added bit vector encoding for Q1.

--- context/q1_bench.cpp
+++ context/q1_bench.cpp
@@ -4,9 +4,11 @@
 #include "huffman.h"
 #include <pthread.h>
 #include <random>
+#include <ibis.h>
 
 #define NUM_THREADS 4
 //#define HUFFMAN
+#define FASTBIT
 
 namespace benchmark
 {
@@ -20,6 +22,10 @@ namespace benchmark
     Node<unsigned short>* p1_tree;
 #endif
     
+#ifdef FASTBIT
+    ibis::bitvector** p1_terms_fragments_compressed;
+#endif
+    
     int* p2_docs;
     int** p2_docs_fragments;
     
@@ -29,6 +35,11 @@ namespace benchmark
     char** p2_docs_fragments_compressed;
     Node<int>* p2_tree;
 #endif
+    
+#ifdef FASTBIT
+    ibis::bitvector** p2_docs_fragments_compressed;
+#endif
+    
     
     int** exact_docs_oo;
     
@@ -94,6 +105,33 @@ namespace benchmark
         delete[] p1_terms_fragments;
 #endif
     
+#ifdef FASTBIT
+        show_info("[P1] Compressing with FastBit...");
+        p1_terms_fragments_compressed = new ibis::bitvector*[input::D_PM];
+        
+        for (int d = 0; d < input::D_PM; ++d)
+        {
+            //p1_terms_fragments_compressed[d] = new ibis::bitvector();
+            ibis::bitvector* vector = new ibis::bitvector();
+            
+            for (int t_i = 0; t_i < pubmed::get_group_by_doc(d); ++t_i)
+            {
+                vector->setBit(p1_terms_fragments[d][t_i], 1);
+            }
+            
+            vector->compress();
+            // force new allocate
+            ibis::array_t<uint32_t>* arr = new ibis::array_t<uint32_t>();
+            vector->write(*arr);
+            delete vector;
+            p1_terms_fragments_compressed[d] = new ibis::bitvector(*arr);
+            delete arr;
+            
+            delete[] p1_terms_fragments[d];
+        }
+        delete[] p1_terms_fragments;
+#endif
+        
         show_info("[P2] Generating random data...");
         p2_docs = new int[input::NUM_TUPLES];
         index = 0;
@@ -152,6 +190,32 @@ namespace benchmark
         delete[] p2_docs_fragments;
 #endif
         
+#ifdef FASTBIT
+        show_info("[P2] Compressing with FastBit...");
+        p2_docs_fragments_compressed = new ibis::bitvector*[input::T_PM];
+        
+        for (int t = 0; t < input::T_PM; ++t)
+        {
+            ibis::bitvector* vector = new ibis::bitvector();
+            
+            for (int d_i = 0; d_i < pubmed::get_group_by_term(t); ++d_i)
+            {
+                vector->setBit(p2_docs_fragments[t][d_i], 1);
+            }
+            
+            vector->compress();
+            // force new allocate
+            ibis::array_t<uint32_t>* arr = new ibis::array_t<uint32_t>();
+            vector->write(*arr);
+            delete vector;
+            p2_docs_fragments_compressed[t] = new ibis::bitvector(*arr);
+            delete arr;
+            
+            delete[] p2_docs_fragments[t];
+        }
+        delete[] p2_docs_fragments;
+#endif
+        
         show_info("DONE.");
     }
     
@@ -178,14 +242,42 @@ namespace benchmark
 #ifdef HUFFMAN
             decode(p2_docs_fragments_compressed[term], pubmed::get_group_by_term(term), doc_fragment_uncompressed, p2_huffman_array, p2_terminator_array);
 #else
+#ifndef FASTBIT
             doc_fragment_uncompressed = p2_docs_fragments[term];
 #endif
+#endif
             
+#ifndef FASTBIT
             for (int d = 0; d < pubmed::get_group_by_term(term); ++d)
             {
                 (*args->doc_freq)[doc_fragment_uncompressed[d]]++;
                 cntr++;
             }
+#endif
+            
+#ifdef FASTBIT
+            ibis::bitvector* doc_fragment = p2_docs_fragments_compressed[term];
+            ibis::bitvector::indexSet ones = doc_fragment->firstIndexSet();
+            bool is_range = ones.isRange();
+            
+            while (ones.nIndices() != 0)
+            {
+                for (int i = 0; i < ones.nIndices(); ++i)
+                {
+                    if (is_range)
+                    {
+                        (*args->doc_freq)[ones.indices()[0] + i]++;
+                    }
+                    else
+                    {
+                        (*args->doc_freq)[ones.indices()[i]]++;
+                    }
+                    cntr++;
+                }
+                
+                ++ones;
+            }
+#endif
             
 #ifdef HUFFMAN
             delete[] doc_fragment_uncompressed;
@@ -219,7 +311,33 @@ namespace benchmark
 #ifdef HUFFMAN
             decode(p1_terms_fragments_compressed[doc], term_cnt, terms_uncompressed, p1_huffman_array, p1_terminator_array);
 #else
+#ifdef FASTBIT
+            ibis::bitvector* compressed_fragment = p1_terms_fragments_compressed[doc];
+            terms_uncompressed = new unsigned short[term_cnt];
+            
+            ibis::bitvector::indexSet ones = compressed_fragment->firstIndexSet();
+            int one_index = 0;
+            bool is_range = ones.isRange();
+            
+            while (ones.nIndices() != 0)
+            {
+                for (int i = 0; i < ones.nIndices(); ++i)
+                {
+                    if (is_range)
+                    {
+                        terms_uncompressed[one_index++] += ones.indices()[0] + i;
+                    }
+                    else
+                    {
+                        terms_uncompressed[one_index++] += ones.indices()[i];
+                    }
+                }
+                
+                ++ones;
+            }
+#else
             terms_uncompressed = p1_terms_fragments[doc];
+#endif
 #endif
             
             pthread_t** threads = new pthread_t*[NUM_THREADS];
